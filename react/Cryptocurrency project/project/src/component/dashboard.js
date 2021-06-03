41c8512fdc2af56ef11e4a6fcc1dc609c776cbfb
Pleumjai.official <pleumjai.official@gmail.com>
2021-06-03 15:40:24 +0700

    V8
    update dashboard

--- react/Cryptocurrency project/project/src/component/dashboard.js
+++ react/Cryptocurrency project/project/src/component/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Line, line } from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2';
 import Axios from 'axios'
 import { useState , useEffect, state} from 'react'
 import Footer from './Footer-fixed';
@@ -27,21 +27,53 @@ function Dashboard() {
         getPayment();
     }, []);
 
+    const coin = {
+        labels: [
+            'Bitcoin',
+            'ETHEREUM',
+            'BINANCE COIN',
+            'CARDANO',
+            'dummy coin1',
+            'dummy coin2',
+            'dummy coin3'
+        ],
+        datasets: [{
+            
+            label: 'Max value',
+            data: [65, 59, 90, 81, 96, 55, 70],
+            fill: true,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: 'rgb(255, 99, 132)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }, {
+            label: 'Min value',
+            data: [28, 48, 40, 19, 46, 27, 20],
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+        }]
+    };
+
     return (
         <div className="container dashboard_view">
             <div className="row">
                 <div className="col">
                     <div className="row">
                         <div className="col mt-2">
-                            <h3>PonCoin</h3>
-                        </div>
-                        <div className="col text-price">
-                            <p>Last Price</p>
-                            <p>1 PonCoin = 100 บาท</p>
+                            <h3>BIG PICTURE</h3>
                         </div>
                     </div>
                     <div className = "chart-dashboard">
-                        {/* <Line data = {dashboard}/> */}
+                            <Radar
+                                data={coin}
+                            />
                     </div>
                 </div>
                 <div className="col">
